0a4cdee98d5926baa6118ca6598d7b3f57f0c90b
binbinmods <binbinmods@gmail.com>
2025-02-11 12:10:07 -0500

    v1.1 fixes to curing heal and elemental roar

--- Rosalinde/CHANGELOG.md
+++ Rosalinde/CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.1.0
+
+Fixed Curing Heal not working well in the forge/with level ups
+
+Fixed Elemental Roar damage.
+
 # 1.0.0
 
 Initial release.
--- RosalindeTraits/obj/Debug/netstandard2.1/PluginInfo.cs
+++ RosalindeTraits/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace Rosalinde
     {
         public const string PLUGIN_GUID = "com.binbin.Rosalinde";
         public const string PLUGIN_NAME = "Rosalinde";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
